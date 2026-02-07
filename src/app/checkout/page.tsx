9b08b5e98b8e9d20c132e2f607aea95a757f7b4f
engsreynich <esreynich6@gmail.com>
2026-02-07 14:30:28 +0700

    asdfghj

--- src/app/checkout/page.tsx
+++ src/app/checkout/page.tsx
@@ -5,7 +5,7 @@ import { zodResolver } from '@hookform/resolvers/zod';
 import { checkoutFormSchema, type CheckoutFormValues } from '@/lib/schemas';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
-import { Button } from '@/components/ui/Button';
+import { Button } from '@/components/ui/button';
 import {
   Select,
   SelectContent,
